303b378f9b32aa321f2f0f5ec0942522c119e5d1
Guilherme Augusto de Sousa <31078186+GuiASousa@users.noreply.github.com>
2018-07-05 14:36:10 -0300

    Search Event
    added a new search for events

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,28 @@
-import search from "./charactersearch"
+import search from "./charactersearch";
+import searche from "./eventsearch";
 
-const form = document.querySelectorAll("[data-form]")
+const form = document.querySelectorAll("[data-form]");
 
 form[0].addEventListener("submit", event => {
-  event.preventDefault()
+  event.preventDefault();
 
-  const formComponent = event.target
+  const formComponent = event.target;
 
-  const formData = new FormData(formComponent)
+  const formData = new FormData(formComponent);
 
-  search(formData.get("Hero"))
-})
+  search(formData.get("Hero"));
+});
+
+const form2 = document.querySelectorAll("[data-form2]");
+
+form2[0].addEventListener("submit", event => {
+  event.preventDefault();
+
+  const formComponent = event.target;
+
+  const formData = new FormData(formComponent);
+
+  searche(formData.get("Event"));
+});
+
+// function toggle()
--- assets/js/eventsearch.js
+++ assets/js/eventsearch.js
@@ -6,10 +6,10 @@ export default name => {
   const publicKey = "4ce3a8f86d1fc6c4f8a45dd8b2375e9b";
   const privateKey = "0ee0eceaca30c0c2ad606758f08454c582b4c8d8";
   const hash = md5(`${ts}${privateKey}${publicKey}`);
-  const hero = name;
+  const event = name;
 
   const instance = axios.create({
-    baseURL: `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${hero}&apikey=${publicKey}&hash=${hash}`,
+    baseURL: `https://gateway.marvel.com:443/v1/public/events?nameStartsWith=${event}&apikey=${publicKey}&hash=${hash}`,
     timeout: 10000
   });
   instance
